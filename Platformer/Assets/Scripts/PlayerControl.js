9b2d643b55cfd3b19944ef4e35bb37d6a298d2c8
PrincessFaerie <arschlag@gmail.com>
2015-10-01 10:35:33 -0600

    SpinScript-Level5-LeaveWallScript

--- Platformer/Assets/Scripts/PlayerControl.js
+++ Platformer/Assets/Scripts/PlayerControl.js
@@ -62,7 +62,7 @@ function OnCollisionEnter2D(coll : Collision2D){
  //consider when character is jumping .. it will exit collision.
 function OnCollisionExit2D(coll : Collision2D){
 	if(coll.gameObject.tag == "wall"){
-		//isgrounded = false;
+		isgrounded = false;
 	}
 }
 
--- Platformer/Assets/Scripts/spinScript.js
+++ Platformer/Assets/Scripts/spinScript.js
@@ -4,8 +4,9 @@ var speed : float;
 
 function Start () {
 	GetComponent.<Rigidbody2D>().rotation = speed;
+	
 }
 
 function Update () {
-
+	GetComponent.<Rigidbody2D>().rotation = GetComponent.<Rigidbody2D>().rotation +speed;
 }
